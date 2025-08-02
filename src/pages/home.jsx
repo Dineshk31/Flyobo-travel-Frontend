c17fcf0f366d9082f1a33b078c275f4f2a0fdce7
Dinesh <131765170+Dineshk31@users.noreply.github.com>
2025-08-02 23:29:34 +0530

    created reusable Button component and used in Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,10 @@
 import Hero from "../components/hero.jsx";
+import Button from "../components/Button.jsx"
 
 export default function Home() {
   return (
     <div>
+      <Button onClick={() => alert("Booking started!")}>Book Now</Button>
       <Hero />
     </div>
   );
